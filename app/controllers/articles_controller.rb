1162338050a401229b79cba56907acf26b1315fc
es-lee <dldms800@gmail.com>
2015-03-30 20:33:39 +0900

    save data in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,18 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		render plain: params[:article].inspect
+		@article = Article.new(article_params)
+		#initialize new model with filtered parameter.
+		@article.save
+		#store the model to DB
+		#Returns success or failure as a boolean value
+		redirect_to @article
+		#redirect automatically to 'show' action, defined later.
+	end
+
+	private
+	def article_params
+		params.require(:article).permit(:title, :text)
+		#whitelist our controller parameters to prevent wrongful mass assignment
 	end
 end
